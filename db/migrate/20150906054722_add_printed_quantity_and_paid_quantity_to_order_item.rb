b6b9ee6683db5117595907b887e964961382dd42
evan <ev.kristian@gmail.com>
2015-09-06 12:51:18 +0700

    add printed and paid quantity

--- db/migrate/20150906054722_add_printed_quantity_and_paid_quantity_to_order_item.rb
+++ db/migrate/20150906054722_add_printed_quantity_and_paid_quantity_to_order_item.rb
@@ -1,6 +1,6 @@
 class AddPrintedQuantityAndPaidQuantityToOrderItem < ActiveRecord::Migration
   def change
-    add_column :order_items, :paid_quantity, :integer
-    add_column :order_items, :printed_quantity, :integer
+    add_column :order_items, :paid_quantity, :integer, default: 0
+    add_column :order_items, :printed_quantity, :integer, default: 0
   end
 end
