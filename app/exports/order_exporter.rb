35b444ca10695040edb2aa11d5c8e9d8820a7ab2
evan <ev.kristian@gmail.com>
2015-09-10 02:06:07 +0700

    add scheduler

--- app/exports/order_exporter.rb
+++ app/exports/order_exporter.rb
@@ -3,13 +3,13 @@ module Exports
     include HTTParty
 
     def initialize
-      @base_uri = 'localhost:3000/v1'
+      @base_uri = 'localhost:4000/v1'
     end
 
     def do_export
       outlet      = Outlet.first
-      orders      = Order.all #where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-      order_items = OrderItem.all #where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+      orders      = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+      order_items = OrderItem.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
       data        = { orders: orders, order_items: order_items, outlet: outlet }
 
       respose     = HTTParty.post("http://#{@base_uri}/orders/import", { body: data.to_json })
