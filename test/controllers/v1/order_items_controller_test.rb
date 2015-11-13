e5f46f54099ccc5d772c16f47e4f84ae5fe67060
Cecep Barkah <maceonrails@gmail.com>
2015-11-13 15:03:11 +0700

    fix gitignore

---
+++ test/controllers/v1/order_items_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class V1::OrderItemsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/v1/product_categories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class V1::ProductCategoriesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
