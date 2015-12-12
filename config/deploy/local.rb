4e57f7bfd219b58a9a4d82a1daf0c923bc4a98be
cecep barkah <maceonrails@gmail.com>
2015-12-12 13:27:29 +0700

    change port

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -15,7 +15,7 @@ role :db,  %w{192.168.1.253}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '192.168.1.253', user: 'cecep', roles: %w{web app}
+server '192.168.1.253', user: 'cecep', roles: %w{web app}, port: 3333
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
