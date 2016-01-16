31fc20e4807442df8ca97ad3649ce55ebd8fb877
cecep barkah <maceonrails@gmail.com>
2016-01-16 14:15:02 +0700

    change scheduler

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,6 @@ set :branch, :savenue
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for :scm is :git
 # set :scm, :git
--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -18,6 +18,8 @@ role :db,  %w{savenue.ddns.ms}
 server 'savenue.ddns.ms', user: 'cecep', roles: %w{web app}, port: 22
 set :deploy_to, '/var/www/xsquare'
 
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
